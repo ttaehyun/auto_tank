63c46d31ee411c1ad1f583e244a9f425d51f71a3
ttaehyun <rth0824@naver.com>
2023-07-04 00:11:32 +0900

    gitignore, track_move

--- auto_tank_test.py
+++ auto_tank_test.py
@@ -11,35 +11,30 @@ Stop = 2
 
 HIGH = 1
 LOW = 0
-class Gpio_setting:
-    def __init__(self, En, Ina, Inb):
-        self.en = En
-        self.ina = Ina
-        self.inb = Inb
-    def setPinConfig(self):
-        GPIO.setup(self.en, GPIO.OUT)
-        GPIO.setup(self.ina, GPIO.OUT)
-        GPIO.setup(self.inb, GPIO.OUT)
-        pwm = GPIO.PWM(self.en, 100)
-        pwm.start(0)
-        return pwm
-class Motor(Gpio_setting):
-      def __init__(self,En, Ina, Inb):
-          super().__init__(En, Ina, Inb)
-          self.speed = 0
-          self.stat = 0
 
-      def setMotor(self, pwm, speed, stat):
-          pwm.ChangeDutyCycle(speed)
-          if stat == Forward:
-               GPIO.output(self.Ina, LOW)
-               GPIO.output(self.Inb, HIGH)
-          elif stat == Backward:
-               GPIO.output(self.Ina, HIGH)
-               GPIO.output(self.Inb, LOW)
-          elif stat == Stop:
-               GPIO.output(self.Ina, LOW)
-               GPIO.output(self.Inb, LOW)
+def setPinConfig(en, ina, inb):
+    GPIO.setup(en, GPIO.OUT)
+    GPIO.setup(ina, GPIO.OUT)
+    GPIO.setup(inb, GPIO.OUT)
+    pwm = GPIO.PWM(en, 100)
+    pwm.start(0)
+    return pwm
+class Motor():
+    def __init__(self,En, Ina, Inb):
+        self.speed = 0
+        self.stat = 0
+        self.pwm = setPinConfig(En, Ina, Inb)
+    def setMotor(self, speed, elec_stat):
+        self.pwm.ChangeDutyCycle(speed)
+        if elec_stat == Forward:
+            GPIO.output(self.Ina, LOW)
+            GPIO.output(self.Inb, HIGH)
+        elif elec_stat == Backward:
+            GPIO.output(self.Ina, HIGH)
+            GPIO.output(self.Inb, LOW)
+        elif elec_stat == Stop:
+            GPIO.output(self.Ina, LOW)
+            GPIO.output(self.Inb, LOW)
 
 Left_track = Motor(32, 38, 26)
 Right_track = Motor(33, 37, 35)
@@ -47,5 +42,7 @@ turret_rotation = Motor(24, 16, 22)
 gun_tilt = Motor(19, 21, 23)
 gun_reload = Motor(27, 29, 31)
 
+Left_track.setMotor(50, Forward)
+Right_track.setMotor(50, Forward)
 
 GPIO.cleanup()
