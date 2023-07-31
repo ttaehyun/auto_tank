598d9ba25aae0a337d2298c9efd20ad5a2614b1b
ttaehyun <rth0824@naver.com>
2023-07-31 20:22:12 +0900

    원격제어 확인

--- keytest.py
+++ keytest.py
@@ -44,41 +44,49 @@ turret_rotation = Motor(24, 16, 22)
 gun_tilt = Motor(19, 21, 23)
 gun_reload = Motor(27, 29, 31)
 
-def keyboard_control(screen):
-	key = screen.getch()
-	if key == curses.KEY_UP:
-		return "up"
-	elif key == curses.KEY_DOWN:
-		return "down"
-	elif key == curses.KEY_LEFT:
-		return "left"
-	elif key == curses.KEY_RIGHT:
-		return "right"
-	else:
-		return "stop"
-	
 def keymap(screen):
 	screen.clear()
 	screen.refresh()
 	try:
 		while True :
-			test = keyboard_control(screen)		
-			
+
+			curses.halfdelay(5)
+			key = screen.getch()
 			#key 값이 입력이 없으면 계속 위 코드에서 정지해서 밑에게 실행이 안된거임
 			# 0으로 반환하는 그런걸 찾아야할듯
-			if test == "up":
+			if key == curses.KEY_UP:
 				Left_track.setMotor(60, Forward)
 				Right_track.setMotor(60,Forward)
-			elif test == "down":
+			elif key == curses.KEY_DOWN:
 				Left_track.setMotor(60,Backward)
 				Right_track.setMotor(60,Backward)
-			elif test == "left":
+			elif key == curses.KEY_LEFT:
 				Left_track.setMotor(60,Backward)
 				Right_track.setMotor(60,Forward)
-			elif test == "right":
+			elif key == curses.KEY_RIGHT:
 				Left_track.setMotor(60, Forward)
 				Right_track.setMotor(60, Backward)
-			
+			elif key == 119:
+				#주포 윗방향
+				gun_tilt.setMotor(60, Forward)
+			elif key == 115:
+				#주포 아랫방향
+				gun_tilt.setMotor(60, Backward)
+			elif key == 97:
+				#포탑 왼쪽회전
+				turret_rotation.setMotor(50, Forward)
+			elif key == 100:
+				#포탑 오른쪽회전
+				turret_rotation.setMotor(50, Backward)
+			elif key == 114:
+				#장전
+				gun_reload.setMotor(100, Forward)
+			else:
+				Left_track.setMotor(0, Stop)
+				Right_track.setMotor(0,Stop)
+				gun_reload.setMotor(0,Stop)
+				gun_tilt.setMotor(0, Stop)
+				turret_rotation.setMotor(0, Stop)
 				
 	except KeyboardInterrupt:
 		print("\nCtrl c 중지")
